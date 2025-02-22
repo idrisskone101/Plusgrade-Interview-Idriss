e255d29634ac0e734947bf9c8bb555dc16564f6a
Idriss Kone <idriss.kone@me.com>
2025-02-21 22:46:46 -0500

    react/typescript/tailwindcss frontend setup

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <div className="bg-red-500">
+      <h1 className="text-3xl font-bold underline text-white">Hello World</h1>
+    </div>
+  )
+}
+
+export default App
